cb1304ad5a47cfabf9231f842a325137aa7a3df2
James-Robertson101 <153088960+James-Robertson101@users.noreply.github.com>
2025-10-30 14:05:13 +0000

    navbar css done

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,16 +3,14 @@
   <head>
     <title>{{$title}}</title>
     <meta http-equiv="content-type" content="text/html;charset=utf-8" />
-    <link href="{{asset('css/style.css')}}" type="text/css" rel="stylesheet" />
+    <link href="{{asset('css/layout.css')}}" type="text/css" rel="stylesheet" />
   </head>
   <body>
-    <nav>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/create">Add new Opening</a></li>
-        <li><a href="/about">About</a></li>
-      </ul>
-    </nav>
+    <div class="topnav">
+      <a class = "navlink" href="/">Home</a>
+      <a class = "navlink" href="/create">Add new Opening</a>
+      <a class = "navlink" href="/about">About</a>
+    </div>
     {{$slot}}
   </body>
 </html>
